fdefa892e9ca1b684d372f2d769c2c3fc798f835
hayate <hayate.k.0704@gmail.com>
2025-05-06 20:15:30 +0900

    コミット漏れ

--- lib/core/utils/platform_route.dart
+++ lib/core/utils/platform_route.dart
@@ -1,16 +1,12 @@
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/foundation.dart' show kIsWeb;
-import 'dart:io'
-    if (dart.library.html) 'package:goal_timer/core/utils/platform_stub.dart';
 
 Route<dynamic> platformPageRoute({
   required WidgetBuilder builder,
   RouteSettings? settings,
 }) {
-  if (kIsWeb) {
-    return MaterialPageRoute(builder: builder, settings: settings);
-  } else if (Platform.isIOS) {
+  if (Platform.isIOS) {
     return CupertinoPageRoute(builder: builder, settings: settings);
   } else {
     return MaterialPageRoute(builder: builder, settings: settings);
