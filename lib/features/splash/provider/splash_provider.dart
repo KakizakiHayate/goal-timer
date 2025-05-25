270604f45e066f90eeeed4dd2eb3e42a18c367ba
hayate <hayate.k.0704@gmail.com>
2025-05-25 19:40:33 +0900

    viewmodelを修正

--- lib/features/splash/provider/splash_provider.dart
+++ lib/features/splash/provider/splash_provider.dart
@@ -1,5 +1,5 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:goal_timer/features/splash/presentation/viewmodels/splash_view_model.dart';
+import 'package:goal_timer/features/splash/presentation/view_models/splash_view_model.dart';
 
 final splashViewModelProvider =
     StateNotifierProvider<SplashViewModel, SplashState>((ref) {
